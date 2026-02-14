62a6a503ac6b094c3ceefa0e5cd1c95d179be381
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-02-14 15:45:54 +0100

    Add linked list management functions

--- SparseMatrix/SparseMatrix.h
+++ SparseMatrix/SparseMatrix.h
@@ -24,4 +24,14 @@ void PutValue(SparseMatrix* matrix, int linePosition, int columnPosition, int va
 void AddMatrix(SparseMatrix* a, SparseMatrix* b);
 int ComputeMemoryGain(SparseMatrix* matrix);
 
+SparseMatrix* CreateSparseMatrix(int lineCount, int columnCount);
+MatrixElement* CreateMatrixElement(int value, int column);
+MatrixLine* CreateMatrixLines(int size);
+
+void UpdateSparseLine(MatrixLine line, MatrixElement* elementToInsert);
+
+void DeleteMatrix(SparseMatrix* matrix);
+void DeleteMatrixLine(MatrixLine line);
+void DeleteElement(MatrixElement* element);
+
 #endif
