d74335af71d861470d35c50fbbdf560d3d1f7858
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-02-14 13:29:35 +0100

    Define initial API as per instructions

---
+++ SparseMatrix/SparseMatrix.h
@@ -0,0 +1,27 @@
+#ifndef SPARSEMATRIX_H_INCLUDED
+#define SPARSEMATRIX_H_INCLUDED
+
+typedef struct element
+{
+    int column;
+    int value;
+    struct element *nextElement;
+}MatrixElement;
+
+typedef MatrixElement *MatrixLine;
+
+typedef struct matrix{
+    MatrixLine* lines;
+    int lineCount;
+    int columnCount;
+}SparseMatrix;
+
+void FillMatrix(SparseMatrix *matrix, int lineCount, int columnCount);
+void DisplayMatrixAsTable(SparseMatrix *matrix);
+void DisplayMatrixAsLists(SparseMatrix* matrix);
+int GetValue(SparseMatrix* matrix, int linePosition, int columnPosition);
+void PutValue(SparseMatrix* matrix, int linePosition, int columnPosition, int value);
+void AddMatrix(SparseMatrix* a, SparseMatrix* b);
+int ComputeMemoryGain(SparseMatrix* matrix);
+
+#endif
