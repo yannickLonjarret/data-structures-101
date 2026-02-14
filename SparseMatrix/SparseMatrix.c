3d05c1009b622ba187a4bbd8fe7505e879d9bd94
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-02-14 15:47:00 +0100

    Add linked list creation implementation

--- SparseMatrix/SparseMatrix.c
+++ SparseMatrix/SparseMatrix.c
@@ -3,6 +3,20 @@
 #include "SparseMatrix.h"
 
 
+MatrixLine* CreateMatrixLines(int size){
+    MatrixLine* lines = (MatrixLine*)malloc(sizeof(MatrixLine)*size);
+
+    if(lines == NULL){
+        printf("Malloc issue when creating the matrix lines.");
+        return NULL;
+    }
+
+    for(int i = 0; i < size; i++)
+        lines[i] = NULL;
+
+    return lines;
+}
+
 MatrixElement* CreateMatrixElement(int value, int column){
     MatrixElement* elem = (MatrixElement*)malloc(sizeof(MatrixElement));
 
