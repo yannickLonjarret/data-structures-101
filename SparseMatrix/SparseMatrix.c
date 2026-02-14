71a3d07c911063fe9f52fd7f933ae4c7d5d12cde
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-02-14 15:46:30 +0100

    Add element creation implementation

--- SparseMatrix/SparseMatrix.c
+++ SparseMatrix/SparseMatrix.c
@@ -2,3 +2,20 @@
 #include <stdio.h>
 #include "SparseMatrix.h"
 
+
+MatrixElement* CreateMatrixElement(int value, int column){
+    MatrixElement* elem = (MatrixElement*)malloc(sizeof(MatrixElement));
+
+    if(elem == NULL){
+        printf("Malloc issue when creating an element.");
+        return NULL;
+    }
+
+    elem->column=column;
+    elem->value=value;
+    elem->nextElement = NULL;
+
+    return elem;
+}
+
+
