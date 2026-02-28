16283e78679bbdbfdef750f8bc03f603272f8d37
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-02-28 18:54:12 +0100

    add element insertion and removal tests in runner

--- Tests/SparseMatrixTest/SparseMatrixTestRunner.c
+++ Tests/SparseMatrixTest/SparseMatrixTestRunner.c
@@ -6,6 +6,13 @@ extern void test_Create_Matrix_Element(void);
 extern void test_Free_Matrix_Element(void);
 extern void test_Create_Matrix_Lines(void);
 extern void test_Create_Sparse_Matrix(void);
+extern void test_Insert_Element_At_Head(void);
+extern void test_Insert_Element_At_Tail(void);
+extern void test_Insert_Element_At_Middle(void);
+extern void test_Remove_Element_At_Head_Empty_Line(void);
+extern void test_Remove_Element_At_Head_Filled_Line(void);
+extern void test_Remove_Element_At_Tail(void);
+extern void test_Remove_Element_At_Middle(void);
 
 
 int main(void)
@@ -16,5 +23,13 @@ int main(void)
     RUN_TEST(test_Create_Matrix_Lines);
     RUN_TEST(test_Create_Sparse_Matrix);
 
+    RUN_TEST(test_Insert_Element_At_Head);
+    RUN_TEST(test_Insert_Element_At_Tail);
+    RUN_TEST(test_Insert_Element_At_Middle);
+
+    RUN_TEST(test_Remove_Element_At_Head_Empty_Line);
+    RUN_TEST(test_Remove_Element_At_Head_Filled_Line);
+    RUN_TEST(test_Remove_Element_At_Tail);
+    RUN_TEST(test_Remove_Element_At_Middle);
     return UNITY_END();
 }
