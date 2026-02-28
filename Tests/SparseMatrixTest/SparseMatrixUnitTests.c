b94e38beb264f628c204cdb3fd216810160f74d1
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-02-28 18:49:02 +0100

    Add memory dealloc in test

--- Tests/SparseMatrixTest/SparseMatrixUnitTests.c
+++ Tests/SparseMatrixTest/SparseMatrixUnitTests.c
@@ -16,6 +16,8 @@ void test_Create_Matrix_Element(void)
     TEST_ASSERT_NOT_NULL(element);
     TEST_ASSERT_EQUAL_INT(2, element->column);
     TEST_ASSERT_EQUAL_INT(1, element->value);
+
+    DeleteElement(&element);
 }
 
 void test_Free_Matrix_Element(void)
@@ -33,14 +35,20 @@ void test_Create_Matrix_Lines(void)
     for(int i = 0; i < 3; i++)
     {
         TEST_ASSERT_NULL(lines[i]);
+        DeleteMatrixLine(&lines[i]);
     }
+}
 }
 
 void test_Create_Sparse_Matrix(void)
 {
     SparseMatrix* matrix = CreateSparseMatrix(3, 3);
+    
     TEST_ASSERT_NOT_NULL(matrix);
     TEST_ASSERT_NOT_NULL(matrix->lines);
     TEST_ASSERT_EQUAL_INT(3, matrix->lineCount);
     TEST_ASSERT_EQUAL_INT(3, matrix->columnCount);
+
+    DeleteMatrix(&matrix);
+}
 }
