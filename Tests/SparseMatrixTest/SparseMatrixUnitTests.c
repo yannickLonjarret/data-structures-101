90e2295a8ec7e15412e4eac259151f24a620f75a
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-02-28 18:51:39 +0100

    Add element removal unit tests

--- Tests/SparseMatrixTest/SparseMatrixUnitTests.c
+++ Tests/SparseMatrixTest/SparseMatrixUnitTests.c
@@ -113,4 +113,79 @@ void test_Insert_Element_At_Middle(void){
     
     DeleteMatrixLine(&lines[0]);
 }
+
+void test_Remove_Element_At_Head_Empty_Line(void){
+    MatrixLine* lines = CreateMatrixLines(1);
+    MatrixElement* element = CreateMatrixElement(1, 2);
+
+    UpdateSparseLine(&lines[0], element);
+    RemoveSparseLineElement(&lines[0], 2);
+
+    TEST_ASSERT_NULL(lines[0]);
+
+    DeleteMatrixLine(&lines[0]);
+}
+
+void test_Remove_Element_At_Head_Filled_Line(void){
+    MatrixLine* lines = CreateMatrixLines(1);
+    MatrixElement* element = CreateMatrixElement(1, 2);
+
+    UpdateSparseLine(&lines[0], element);
+    element = CreateMatrixElement(1, 4);
+    UpdateSparseLine(&lines[0], element);
+
+    RemoveSparseLineElement(&lines[0], 2);
+
+    TEST_ASSERT_NOT_NULL(lines[0]);
+    TEST_ASSERT_NULL(lines[0]->nextElement);
+
+    TEST_ASSERT_EQUAL_INT(4, lines[0]->column);
+    TEST_ASSERT_EQUAL_INT(1, lines[0]->value);
+
+    DeleteMatrixLine(&lines[0]);
+}
+
+void test_Remove_Element_At_Tail(void){
+    MatrixLine* lines = CreateMatrixLines(1);
+    MatrixElement* element = CreateMatrixElement(1, 2);
+
+    UpdateSparseLine(&lines[0], element);
+    element = CreateMatrixElement(1, 4);
+    UpdateSparseLine(&lines[0], element);
+
+    RemoveSparseLineElement(&lines[0], 4);
+
+    TEST_ASSERT_NOT_NULL(lines[0]);
+    TEST_ASSERT_NULL(lines[0]->nextElement);
+
+    TEST_ASSERT_EQUAL_INT(2, lines[0]->column);
+    TEST_ASSERT_EQUAL_INT(1, lines[0]->value);
+
+    DeleteMatrixLine(&lines[0]);
+}
+
+void test_Remove_Element_At_Middle(void){
+    MatrixLine* lines = CreateMatrixLines(1);
+    MatrixElement* element = CreateMatrixElement(1, 2);
+    UpdateSparseLine(&lines[0], element);
+
+    element = CreateMatrixElement(1, 4);
+    UpdateSparseLine(&lines[0], element);
+
+    element = CreateMatrixElement(1, 3);
+    UpdateSparseLine(&lines[0], element);
+
+    RemoveSparseLineElement(&lines[0], 3);
+
+    TEST_ASSERT_NOT_NULL(lines[0]);
+    TEST_ASSERT_NOT_NULL(lines[0]->nextElement);
+    TEST_ASSERT_NULL(lines[0]->nextElement->nextElement);
+
+    TEST_ASSERT_EQUAL_INT(2, lines[0]->column);
+    TEST_ASSERT_EQUAL_INT(1, lines[0]->value);
+
+    TEST_ASSERT_EQUAL_INT(4, lines[0]->nextElement->column);
+    TEST_ASSERT_EQUAL_INT(1, lines[0]->nextElement->value);
+    
+    DeleteMatrixLine(&lines[0]);
 }
