61c2ce1e7863bb06d5b2ffc5da9ecd46028cf6c0
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-02-28 18:49:41 +0100

    add nextElement test in Element creation

--- Tests/SparseMatrixTest/SparseMatrixUnitTests.c
+++ Tests/SparseMatrixTest/SparseMatrixUnitTests.c
@@ -13,7 +13,9 @@ void tearDown(void)
 void test_Create_Matrix_Element(void)
 {
     MatrixElement* element = CreateMatrixElement(1, 2);
+
     TEST_ASSERT_NOT_NULL(element);
+    TEST_ASSERT_NULL(element->nextElement);
     TEST_ASSERT_EQUAL_INT(2, element->column);
     TEST_ASSERT_EQUAL_INT(1, element->value);
 
