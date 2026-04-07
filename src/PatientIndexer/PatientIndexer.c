b9d808ea31eb0d9470515b22cd205849872ccf2b
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-04-07 20:50:27 +0200

    Add partial CreateIndexerManager function

--- src/PatientIndexer/PatientIndexer.c
+++ src/PatientIndexer/PatientIndexer.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "PatientIndexer.h"
 
@@ -15,4 +16,47 @@ IndexerManager CreateIndexerManager(int numberOfIndexers) {
     }
 
     return manager;
+}
+
+PatientFile* CreatePatient(char* lastName, char* firstName) {
+    PatientFile* patient = (PatientFile*)malloc(sizeof(PatientFile));
+
+    if(patient == NULL) {
+        fprintf(stderr, "Malloc issue when creating the patient file.");
+        abort();
+    }
+
+    patient->lastName = NULL;
+    patient->firstName = NULL;
+
+    patient->lastName = (char*)malloc(sizeof(char) * strlen(lastName) + 1);
+    patient->firstName = (char*)malloc(sizeof(char) * strlen(firstName) + 1);
+
+    if(patient->lastName == NULL || patient->firstName == NULL) {
+        fprintf(stderr, "Malloc issue when creating the patient file.");
+        abort();
+    }
+
+    int errorCode = 0;
+    errorCode = snprintf(patient->lastName, strlen(lastName) + 1, "%s", lastName);
+    if(errorCode != 0) {
+        fprintf(stderr, "Error when copying the patient last name.");
+        // DeletePatientFile(&patient);
+        return NULL;
+    }
+
+    errorCode = snprintf(patient->firstName, strlen(firstName) + 1, "%s", firstName);
+    if(errorCode != 0) {
+        fprintf(stderr, "Error when copying the patient first name.");
+        // DeletePatientFile(&patient);
+        return NULL;
+    }
+
+    patient->appointmentCount = 0;
+    patient->appointments = NULL;
+
+    patient->leftPatient = NULL;
+    patient->rightPatient = NULL;
+
+    return patient;
 }
