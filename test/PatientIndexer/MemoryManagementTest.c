fa887a14fd0ac5810154d9071d01021f3e766f0e
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-04-26 12:45:41 +0200

    Memory management test group are file

---
+++ test/PatientIndexer/MemoryManagementTest.c
@@ -0,0 +1,9 @@
+#include "PatientIndexer.h"
+#include "unity.h"
+#include "unity_fixture.h"
+
+TEST_GROUP(MemoryManagement);
+
+TEST_SETUP(MemoryManagement) {}
+
+TEST_TEAR_DOWN(MemoryManagement) {}
