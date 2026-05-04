40662a13a026ceb117b2f9c3b400c30d906d5048
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-05-04 19:03:10 +0200

    Add DeepCopyPatient, no appointment test

--- test/PatientIndexer/DeepCopyTest.c
+++ test/PatientIndexer/DeepCopyTest.c
@@ -12,3 +12,17 @@ TEST(DeepCopy, DeepCopyPatient_NULLOriginal) {
     PatientFile* copy = DeepCopyPatient(NULL);
     TEST_ASSERT_NULL(copy);
 }
+
+TEST(DeepCopy, DeepCopyPatient_NoAppointment) {
+    PatientFile* original = CreatePatient("D", "Test");
+    PatientFile* copy = DeepCopyPatient(original);
+    TEST_ASSERT_NOT_NULL(copy);
+
+    TEST_ASSERT_NOT_EQUAL(original, copy);
+    TEST_ASSERT_EQUAL_STRING(original->firstName, copy->firstName);
+    TEST_ASSERT_EQUAL_STRING(original->lastName, copy->lastName);
+    TEST_ASSERT_EQUAL_STRING(original->appointmentCount, copy->appointmentCount);
+
+    DeletePatientFile(original);
+    DeletePatientFile(copy);
+}
