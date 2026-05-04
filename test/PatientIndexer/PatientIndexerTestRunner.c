97e634bfdb90aba3e8ae2e451b77d10b7814e0a6
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-05-04 21:23:35 +0200

    Add DeepCopy to global test runner

--- test/PatientIndexer/PatientIndexerTestRunner.c
+++ test/PatientIndexer/PatientIndexerTestRunner.c
@@ -6,6 +6,7 @@ static void RunAllTests(void) {
     RUN_TEST_GROUP(TreeInsertion);
     RUN_TEST_GROUP(TreeMinimum);
     RUN_TEST_GROUP(TreeSearch);
+    RUN_TEST_GROUP(DeepCopy);
 }
 
 int main(int argc, const char* argv[]) { return UnityMain(argc, argv, RunAllTests); }
