ec9214652cda005d082273f3e4ac6e09e93974af
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-05-05 19:59:49 +0200

    Define DateFormat Test group runner

--- test/Utils/UtilsTest.c
+++ test/Utils/UtilsTest.c
@@ -1,2 +1,14 @@
 #include "unity.h"
 #include "unity_fixture.h"
+
+TEST_GROUP_RUNNER(DateFormat) {
+    RUN_TEST_CASE(DateFormat, TestisDateValid_NULLDate);
+    RUN_TEST_CASE(DateFormat, TestisDateValid_EmptyDate);
+    RUN_TEST_CASE(DateFormat, TestisDateValid_AlphaCharInput);
+    RUN_TEST_CASE(DateFormat, TestisDateValid_BadSeparatorInput);
+    RUN_TEST_CASE(DateFormat, TestisDateValid_InvalidMonth);
+    RUN_TEST_CASE(DateFormat, TestisDateValid_InvalidDay);
+    RUN_TEST_CASE(DateFormat, TestisDateValid_InvalidLeapYearDay);
+    RUN_TEST_CASE(DateFormat, TestisDateValid_InvalidNonLeapYearDay);
+    RUN_TEST_CASE(DateFormat, TestisDateValid_ValidDate);
+}
