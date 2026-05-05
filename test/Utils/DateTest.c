d07323967fbedfe5930d6cf5f8230253b3dadcec
yannickLonjarret <yannick.lonjarret@gmail.com>
2026-05-05 19:32:13 +0200

    Define all isValidDate tests

--- test/Utils/DateTest.c
+++ test/Utils/DateTest.c
@@ -7,3 +7,19 @@ TEST_GROUP(DateFormat);
 TEST_SETUP(DateFormat) {}
 
 TEST_TEAR_DOWN(DateFormat) {}
+
+TEST(DateFormat, TestisDateValid_NULLDate) {}
+
+TEST(DateFormat, TestisDateValid_EmptyDate) {}
+
+TEST(DateFormat, TestisDateValid_GarbageInput) {}
+
+TEST(DateFormat, TestisDateValid_InvalidMonth) {}
+
+TEST(DateFormat, TestisDateValid_InvalidDay) {}
+
+TEST(DateFormat, TestisDateValid_InvalidLeapYearDay) {}
+
+TEST(DateFormat, TestisDateValid_InvalidNonLeapYearDay) {}
+
+TEST(DateFormat, TestisDateValid_ValidDate) {}
